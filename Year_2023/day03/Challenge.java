764ef321ee54fb8cde1dce5107006e0a3016a214
Anyarchiste <luc.cirque2@gmail.com>
2023-12-05 11:02:42 +0100

    enfin

--- Year_2023/day03/Challenge.java
+++ Year_2023/day03/Challenge.java
@@ -3,7 +3,6 @@ package day03;
 import day03.paths;
 import java.io.File;
 import java.util.Scanner;
-import java.util.Dictionary;
 
 /**
  * Challenge
@@ -14,11 +13,29 @@ public class Challenge {
 
         Scanner input = readFile();
 
-        getSpecialCharacter(input);
+        char[][] matrix = new char[10][10]; //140 - 140
 
+        int indexWhile = 0;
         while (input.hasNextLine()) {
-
+            char[] line = input.nextLine().toCharArray();
+            for (int i = 0; i < line.length; i++) {
+                matrix[indexWhile][i] = line[i];
+            }
+            indexWhile++;
         }
+        input.close();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (checkForSpecialChar(matrix[i][j])) {
+                    
+                } else {
+
+                }
+            }
+        }
+
+
     }
 
     public static Scanner readFile() throws Exception {
@@ -41,4 +58,38 @@ public class Challenge {
 
         return ret;
     }
+
+    public static boolean checkForSpecialChar(char letter) {
+        boolean ret = false;
+        char[] notSpecial = "1234567890.".toCharArray();
+        switch (letter) {
+            case '1':
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+            case '5':
+                break;
+            case '6':
+                break;
+            case '7':
+                break;
+            case '8':
+                break;
+            case '9':
+                break;
+            case '0':
+                break;
+            case '.':
+                break;
+            default:
+                break;
+        }
+
+        return ret;
+    }
+    
 }
