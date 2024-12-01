c95defefbbdbd14ebf00e175c02770cfd44cdcca
anyarchiste <militantisme@lucielle.ch>
2024-12-01 21:50:53 +0100

    2.0 made it so I don't have to put the script in the right folder before launching it

--- createFolders.ps1
+++ createFolders.ps1
@@ -1,17 +1,23 @@
 <#Creates 25 folders named "dayXX" where XX = 01->25
-WARNING it creates the folder in the folder it is in. Make sure you are in
-the right one#>
+WARNING --> Make sure you change the $year variable to the right year
+#>
 
+# Days to christmass
 $A = 25
+
+# Year you do the AOC
+$year = 2024
+
+$yearPath = "./" + $year
+
+New-Item -ItemType Directory -Path $yearPath
 
 for ($i = 1; $i -lt ($A + 1); $i++) {
     $path = ""
     if ($i -lt 10) {
-        <# Action to perform if the condition is true #>
-        $path = ("./day0" + $i)
+        $path = ($yearPath + "/day0" + $i)
     } elseif ($i -ge 10) {
-        <# Action when this condition is true #>
-        $path = ("./day" + $i)
+        $path = ($yearPath + "/day" + $i)
     }
 
     New-Item -ItemType Directory -Path $path
